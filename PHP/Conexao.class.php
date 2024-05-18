70263d870468f87b9763c41b7aed39fa7a449f91
JoãoGSilveira <joaosilveira31@hotmail.com>
2024-05-18 02:04:34 -0300

    Update Site 18-05

--- PHP/Conexao.class.php
+++ PHP/Conexao.class.php
@@ -1,11 +1,38 @@
 <?php
-    abstract class Conexao
-        {
-            public function __construct(protected $db = null)
-            {
-                $parametros = "mysql:host=localhost;dbname=loja;";
 
-                $this->db = new PDO($parametros, "root", "");
-            }
+class Conexao
+{
+    protected $db;
+
+    public function __construct($usuario, $senha)
+    {
+        try {
+            $parametros = "mysql:host=localhost;dbname=petcare;";
+            $this->db = new PDO($parametros, $usuario, $senha);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conexão bem-sucedida!";
+        } catch (PDOException $e) {
+            echo "Erro de conexão: " . $e->getMessage();
+            exit;
         }
-?>
+    }
+
+    public function getDB()
+    {
+        return $this->db;
+    }
+}
+
+$usuario = "root";
+$senha = "";
+$conexao = new Conexao($usuario, $senha);
+
+try {
+    $pdo = $conexao->getDB();
+    $stmt = $pdo->query("SELECT * FROM cliente");
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        print_r($row);
+    }
+} catch (PDOException $e) {
+    echo "Erro ao executar consulta: " . $e->getMessage();
+}
